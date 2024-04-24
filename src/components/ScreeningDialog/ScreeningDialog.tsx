5c33d3c9b00d39c3777a87dfa8235ab6c97c0862
Aldair Cuarez <aldaircuarez98@gmail.com>
2024-04-24 11:29:28 -0500

    Update ScreeningDialog.tsx

--- src/components/ScreeningDialog/ScreeningDialog.tsx
+++ src/components/ScreeningDialog/ScreeningDialog.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
     Dialog,
     DialogClose,
@@ -8,6 +10,9 @@ import {
     DialogTitle
   } from "@/components/ui/dialog"
 import { Button } from '@/components/ui/button'
+import { Checkbox } from "@/components/ui/checkbox"
+import { useState } from "react"
+import { Box, Typography } from "@mui/material"
 
 interface ScreeningDialogProps {
     open: boolean,
@@ -18,12 +23,50 @@ export function ScreeningDialog({
     open,
     onOpenChange
 }: ScreeningDialogProps) {
+    const [ofac, setOfac] = useState(false)
+    const [worldBank, setWorldBank] = useState(false)
+    const [offshoreLeaks, setOffshoreLeaks] = useState(false)
+
+    const sources = [
+        {
+            id: "ofac",
+            name: "OFAC Sanctions List",
+            checked: ofac,
+            onChange: (checked: boolean) => setOfac(checked)
+        },
+        {
+            id: "worldBank",
+            name: "World Bank Listing of Ineligible Firms and Individuals",
+            checked: worldBank,
+            onChange: (checked: boolean) => setWorldBank(checked)
+        },
+        {
+            id: "offshoreLeaks",
+            name: "Offshore Leaks Database",
+            checked: offshoreLeaks,
+            onChange: (checked: boolean) => setOffshoreLeaks(checked)
+        }
+    ]
+
     return <Dialog open={open} onOpenChange={onOpenChange}>
         <DialogContent>
             <DialogHeader>
                 <DialogTitle>Entity Screening</DialogTitle>
                 <DialogDescription>
                     Select the sources for the screening
+                    <Box className="m-3">
+                        {
+                            sources.map((source) => <Box key={source.id} className="flex items-center gap-2 mb-2">
+                                <Checkbox 
+                                    id={source.id}
+                                    checked={source.checked}
+                                    onCheckedChange={source.onChange}
+                                    disabled={source.id === "offshoreLeaks"}
+                                />
+                                <Typography variant="caption">{source.name}</Typography>
+                            </Box>)
+                        }
+                    </Box>
                 </DialogDescription>
             </DialogHeader>
             <DialogFooter className="sm:justify-end">
